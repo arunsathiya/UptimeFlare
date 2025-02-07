9179b6c97df7d31386a1e647effe18d5649d38a1
Arun <arun@arun.blog>
2025-02-07 16:55:47 +0530

    fix: update website monitor request type

--- uptime.config.ts
+++ uptime.config.ts
@@ -23,7 +23,7 @@ const workerConfig = {
       // `name` is used at status page and callback message
       name: 'Website',
       // `method` should be a valid HTTP Method
-      method: 'POST',
+      method: 'GET',
       // `target` is a valid URL
       target: 'https://www.arun.blog',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
